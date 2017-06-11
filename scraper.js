1f22546fd516c8a9b1eccc6ddacbccd5e4f03019
Orock <eviedma@gmail.com>
2017-06-11 19:13:19 -0400

    FIX: se toma el primer dato del dolar

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ const {pool} = require('./db/mysql');
 let scraper = (url) => {
 
   let uf, utm, usd, eur;
-
+  console.log(usd);
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (!error) {
@@ -24,7 +24,9 @@ let scraper = (url) => {
               uf = $(lines[lines.length-1]).html().replace('.', '').replace(',', '.');
               break;
             case 'D&#xF3;lar Observado':
-              usd = $(lines[lines.length-1]).html().replace('.', '').replace(',', '.');
+              if(usd == undefined){ // para tomar el primer dato del dolar
+                usd = $(lines[lines.length-1]).html().replace('.', '').replace(',', '.');
+              }
               break;
             case 'Euro':
               eur = $(lines[lines.length-1]).html().replace('.', '').replace(',', '.');
